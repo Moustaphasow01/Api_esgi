abd09f7e68996c0602d908a05cf381c7dad3a385
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2023-03-30 08:32:23 +0200

    Mis a jour du model commentaire

--- Model/Post.php
+++ Model/Post.php
@@ -6,4 +6,11 @@ class Product extends \Model\Model{
     {
         parent::__construct("product");
     }
+    public function getAllComments()
+    {
+        $req = $this->db->query("SELECT * FROM p");
+        $req->setFetchMode(\PDO::FETCH_OBJ);
+        return $req->fetchAll();
+    }
+    
 }
